167c217f82f7e6227bb81759a426a7cd5c8d2bde
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-20 14:50:33 -0400

    mid commit

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,11 +1,60 @@
 package com.pluralsight;
 
-import java.time.LocalDateTime;
-
 public class Reservation {
-    private String RoomType;
-    private double price;
-    private int NumberOfNights;
-    private LocalDateTime dateTime;
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+    
+    public double getPrice() {
+        double pricePerNight = 0;
+
+        if (roomType.equalsIgnoreCase("king")) {
+            pricePerNight = 139.00;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            pricePerNight = 124.00;
+        }
+
+        if (isWeekend) {
+            pricePerNight = pricePerNight * 1.10;
+        }
+
+        return pricePerNight;
+    }
+
+    public double getReservationTotal() {
+        return getPrice() * numberOfNights;
+    }
+
+
 
 }
