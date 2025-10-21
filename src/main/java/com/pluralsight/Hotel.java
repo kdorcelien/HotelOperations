ba23e9cdc68880e80aa588a98805a9e14b85808e
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-21 15:59:13 -0400

    next day commit

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -6,4 +6,43 @@ public class Hotel {
     private int numberOfRooms;
     private int bookedSuites;
     private int bookedBasicRooms;
+    private boolean bookRoom;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite)
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
 }
