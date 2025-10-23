98060e031fd1b3c7c023dea42aacbc441ba3799b
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-23 10:31:20 -0400

    next day commit 3

--- src/test/java/com/pluralsight/EmployeeTest.java
+++ src/test/java/com/pluralsight/EmployeeTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeTest {
 
     @Test
-    void punchin_should_make_emp_punchin() {
+    void punchin_should_update_hoursworked() {
         Employee emp = new Employee(101, "Mark Twain", "Author", 20.0, 45.0);
 boolean punchIn = true;
         emp.punchIn(8.0);
@@ -16,6 +16,7 @@ boolean punchIn = true;
         assertTrue(emp.getHoursWorked()> 45.0);
         assertFalse(emp.getOvertimeHours()> 45.0);
         assertSame(101, emp.getEmployeeId());
+        assertEquals(20.0,emp.getPayRate());
 
     }
 }
